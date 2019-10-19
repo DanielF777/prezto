25a832266cc8623c8502854895f482c63fd4e24d
Daniel <df254@kent.ac.uk>
2019-10-19 18:06:53 +0100

    Add maven aliases

---
+++ modules/maven/alias.zsh
@@ -0,0 +1,9 @@
+#
+# Defines Maven aliases.
+#
+# Authors:
+#   Daniel Foster <danel.foster262@gmail.com>
+#
+
+alias mi='mvn clean install'
+alias mist='mvn clean install -DskipTests'
---
+++ modules/maven/init.zsh
@@ -0,0 +1,14 @@
+#
+# Defines Maven aliases.
+#
+# Authors:
+#   Daniel Foster <daniel.foster262@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! $+commands[mvn] )); then
+  return 1
+fi
+
+# Source module files.
+source "${0:h}/alias.zsh"
